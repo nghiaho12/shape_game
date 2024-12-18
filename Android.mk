75b5ea349af91c3ebe8e2462d6bf19fd22de80e7
Nghia Ho <nghiaho12@yahoo.com>
2024-12-18 10:16:01 -0800

    fix unintended undo

--- Android.mk
+++ Android.mk
@@ -25,6 +25,6 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include  # SDL
 
 LOCAL_SHARED_LIBRARIES := SDL3
 
-LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -lOpenSLES -llog -landroid  # SDL
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv3 -lOpenSLES -llog -landroid  # SDL
 
 include $(BUILD_SHARED_LIBRARY)
